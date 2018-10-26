57d73da81c73c12cb60e04afb810629c620484a8
Piotr <pwawak@gmail.com>
2018-10-26 14:36:32 +0200

    Initial version

---
+++ IntTests.java
@@ -0,0 +1,16 @@
+/*
+ * find the missing number in a given integer array of 1 to 100?
+ */
+
+import  java.util.Arrays;
+
+public  class IntTests {
+    int     findMissingNumberInIntArray( int[] arr ) {
+        Arrays.sort( arr );
+        for( int i = 0; i < arr.length; i++ )
+            if ( i != arr[i] )
+                return i;
+
+            return 0;
+    }
+}
