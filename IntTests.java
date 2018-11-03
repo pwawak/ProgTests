3cc068385f55851772546a351176953cb31ade53
Piotr <pwawak@gmail.com>
2018-11-03 12:41:20 +0100

    Some unfinished changes

--- IntTests.java
+++ IntTests.java
@@ -3,15 +3,34 @@
  */
 
 import  java.util.Arrays;
+import java.util.BitSet;
 
 public  class IntTests {
-    int     findMissingNumberInIntArray( int[] arr ) {
+    static  int     findMissingNumberInIntArray( int[] arr ) {
         Arrays.sort( arr );
-        for( int i = 0; i < arr.length; i++ )
-            if ( i != arr[i] )
+        for( int i = 1; i <= arr.length; i++ )
+            if ( i != arr[i-1] )
                 return i;
 
             return 0;
+    }
+
+    static  int[]   findMissingNumberInIntArrayBS( int[] arr ) {
+        int     cnt = arr[ arr.length - 1 ];        // input array should be sorted and the last element is the greatest
+        BitSet  bs = new BitSet( cnt );
+
+        for( int i = 0; i < arr.length; i++, cnt-- ) {
+            bs.set( arr[i] - 1 );
+        }
+
+        // now cnt is equal to number of zeros (missing numbers in bitset)
+
+        int[]   rarr = new int [ cnt ];
+
+        for (int i = bs.nextClearBit(0), j=0; i >= 0; i = bs.nextClearBit(i+1), j++)
+            rarr[j] = i;
+
+        return rarr;
     }
 
     int main(String[] args, int argc) {
--- IntTestsTest.java
+++ IntTestsTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Test;
 class IntTestsTest {
     @Test
     void FindingMissingNumbersTest() {
-        assertEquals(2, 1 + 1);
+        assertEquals(5, IntTests.findMissingNumberInIntArray( new int[] {1, 2, 3, 4, 6} ) );
+        assertEquals(5, IntTests.findMissingNumberInIntArray( new int[] {1, 2, 3, 4, 6, 7, 9, 8, 10} ) );
+        assertEquals(5, IntTests.findMissingNumberInIntArray( new int[] {1, 2, 3, 4, 6, 9, 8} ) );
+        assertEquals(5, IntTests.findMissingNumberInIntArray( new int[] {1, 2, 3, 4, 8, 9} ) );
+
+        assertArrayEquals(new int[] { 5 }, IntTests.findMissingNumberInIntArrayBS( new int[] {1, 2, 3, 4, 6} ) );
     }
 }
