3a594a99c22a458246a7f781e7af2352e6ba6ce8
Piotr <Piotr@piotrs-macbook>
2018-11-25 15:53:29 +0100

    Fixed tests for IntTests

--- IntTests.java
+++ IntTests.java
@@ -1,5 +1,5 @@
 /*
- * find the missing number in a given integer array of 1 to 100?
+ * find the missing number in a given integer array of 1 to 100
  */
 
 import  java.util.Arrays;
@@ -16,19 +16,23 @@ public  class IntTests {
     }
 
     static  int[]   findMissingNumberInIntArrayBS( int[] arr ) {
-        int     cnt = arr[ arr.length - 1 ];        // input array should be sorted and the last element is the greatest
+        int     cnt = 1 + arr[ arr.length - 1 ];        // input array should be sorted and the last element is the greatest
         BitSet  bs = new BitSet( cnt );
 
         for( int i = 0; i < arr.length; i++, cnt-- ) {
-            bs.set( arr[i] - 1 );
+            bs.set( arr[i] );
         }
 
-        // now cnt is equal to number of zeros (missing numbers in bitset)
+        // now cnt is equal to the number of zeros (missing numbers in bitset)
 
         int[]   rarr = new int [ cnt ];
 
-        for (int i = bs.nextClearBit(0), j=0; i >= 0; i = bs.nextClearBit(i+1), j++)
-            rarr[j] = i;
+        for (int i=0, j=0; j < rarr.length; i++, j++) {
+        	i = bs.nextClearBit(i);
+        	if( i < 0 )
+        		break;
+        	rarr[j] = i;
+        }
 
         return rarr;
     }
